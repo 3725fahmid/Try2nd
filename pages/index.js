9a21eea1dc893bec7903e557266860c9aaa6b046
Fahmid <fahmidhasankonok@gmail.com>
2021-11-03 10:03:42 +0600

    change look

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,21 @@ export default function Home() {
           Login
         </Button>
       </ul>
+      <div className={styles.FastLook}>
+        <Button className={styles.btn2}>
+          <b>Mobile App</b>
+        </Button>
+        <div className={styles.freecls}>
+          <p>Your Free Class</p>
+          <p>
+            In vector calculus, the gradient of a scalar-valued differentiable
+            function f of several variables is the vector field (or
+            vector-valued function) ∇f whose value at a point p is the vector
+            whose components are the partial derivatives of f at p. That is, for
+            f:ℝⁿ→ℝ, its gradient ∇f:ℝⁿ→ℝⁿ is defined at the …
+          </p>
+        </div>
+      </div>
     </>
   );
 }
